fa861c4c50c819ee1f9a4ad8363ec159cbdbb746
charlene castellani <charlene.castellani13@gmail.com>
2021-03-01 21:41:21 +0100

    clean

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_194019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_194019) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   create_table "voyages", force: :cascade do |t|
     t.string "destination"
@@ -33,7 +34,6 @@ ActiveRecord::Schema.define(version: 2021_03_01_194019) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
   end
 
 end
