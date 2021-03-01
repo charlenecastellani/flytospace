a2ef55b09931f09e206eb222edf831f351f30690
Cathy Perrot <perrot.catherine.13@gmail.com>
2021-03-01 19:48:12 +0100

    cretaion controller voyages

---
+++ app/controllers/voyages_controller.rb
@@ -0,0 +1,2 @@
+class VoyagesController < ApplicationController
+end
---
+++ test/controllers/voyages_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class VoyagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
