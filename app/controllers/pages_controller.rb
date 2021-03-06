b87d736ed81a64c5b9e734894539e0b8838dfc83
Cathy Perrot <perrot.catherine.13@gmail.com>
2021-03-06 11:37:06 +0100

    maj branchevoyage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,2 @@
 class PagesController < ApplicationController
-
-
-
-  def home
-
-  end
-
-#a verifier (amath)
-#def show
- # @voyages = Voyage.find(params[:id])
-#end
-
 end
--- app/controllers/voyages_controller.rb
+++ app/controllers/voyages_controller.rb
@@ -1,4 +1,5 @@
 class VoyagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
 
   def index
     @voyages = Voyage.all
