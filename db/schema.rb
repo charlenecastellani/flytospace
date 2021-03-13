68a2e4d858c4070ee24c756b0961e0573b76a70c
Amath <amathmagnan@gmail.com>
2021-03-13 11:59:34 +0100

    not ok version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_150139) do
+ActiveRecord::Schema.define(version: 2021_03_13_093554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,9 +41,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_150139) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.bigint "voyages_id", null: false
     t.index ["user_id"], name: "index_reservations_on_user_id"
-    t.index ["voyages_id"], name: "index_reservations_on_voyages_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,7 +55,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_150139) do
     t.string "first_name"
     t.string "last_name"
     t.string "agency_name"
-    t.boolean "agency_status", default: false
+    t.boolean "agency_status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -75,6 +73,5 @@ ActiveRecord::Schema.define(version: 2021_03_09_150139) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "reservations", "users"
-  add_foreign_key "reservations", "voyages", column: "voyages_id"
   add_foreign_key "voyages", "users"
 end
